9c699b9c598f5579f963a4084c24561519e417bf
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-09-26 18:59:04 -0500

    seleccionando archivos mejorando

--- regresionMul.py
+++ regresionMul.py
@@ -2,10 +2,22 @@ import streamlit as st
 from sklearn.linear_model import LinearRegression
 import pandas as pd 
 from sklearn.metrics import r2_score,mean_squared_error
+import openpyxl
 
 #Titulo para el app
 st.title('Regresion Lineal Multiple con Streamlit')
 #Elegir archivo a subir
-archivo_subido = st.file_uploader("Selecciona un archivo", type=["csv", "xlsx"])
+archivo_subido = st.file_uploader("Selecciona un archivo: Excel o CSV", type=["csv", "xlsx"])
 
-upload_file = st.file_uploader('Sube un archivo CSV', type=['csv'])
+#VEr Archivo
+if archivo_subido is not None:
+    extension = archivo_subido.split('.')[-1]
+    if extension == 'csv':
+        df = pd.read_csv('archivo_subido')
+        st.write(df.head())
+    elif extension == 'xlsx':
+        df = pd.read_excel('archivo_subido')
+        st.write(df.head())
+    else:
+        st.write('Tipo de archivo no soportado')
+
