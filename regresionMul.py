47348b0be39f4d68202d7d43e78bb5c6b43a0ea7
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-09-26 19:03:06 -0500

    incluir try-exception

--- regresionMul.py
+++ regresionMul.py
@@ -8,16 +8,17 @@ import openpyxl
 st.title('Regresion Lineal Multiple con Streamlit')
 #Elegir archivo a subir
 archivo_subido = st.file_uploader("Selecciona un archivo: Excel o CSV", type=["csv", "xlsx"])
-
-#VEr Archivo
-if archivo_subido is not None:
-    extension = archivo_subido.name.split('.')[-1]
-    if extension == 'csv':
-        df = pd.read_csv('archivo_subido')
-        st.write(df.head())
-    elif extension == 'xlsx':
-        df = pd.read_excel('archivo_subido')
-        st.write(df.head())
-    else:
-        st.write('Tipo de archivo no soportado')
-
+try:
+    #VEr Archivo
+    if archivo_subido is not None:
+        extension = archivo_subido.name.split('.')[-1]
+        if extension == 'csv':
+            df = pd.read_csv('archivo_subido')
+            st.write(df.head())
+        elif extension == 'xlsx':
+            df = pd.read_excel('archivo_subido')
+            st.write(df.head())
+        else:
+            st.write('Tipo de archivo no soportado')
+except Exception as e:
+    st.error(f'Error al leer el archivo: {e}')
