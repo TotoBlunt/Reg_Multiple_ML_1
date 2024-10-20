98157fa24ac8c32ad72ac2b40fd32685bcc08880
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-10-19 20:29:36 -0500

    verifi,.,

--- regresionMul.py
+++ regresionMul.py
@@ -82,9 +82,9 @@ try:
         #Validacion cruzada
         cv_model = cross_val_score(model_RegL,X,y,cv=5,scoring='r2')
         st.write(f'Mediana de Validacion Cruzada: {cv_model.mean():.4f}')
-
-        st.write('### Grafico de dispersion con linea de regresion')
         st.write(y_test.shape,y_pred.shape)
+        st.write('### Grafico de dispersion con linea de regresion')
+        
         #Crear dataframe para el grafico
         results = pd.DataFrame({'Real': y_test, 'Predicción': y_pred})
         #Generacion del grafico con la linea de regresion
